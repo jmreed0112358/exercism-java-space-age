7ce35cc2176d720d60de694982333715d5b63ac8
Jeremy M. Reed <reeje76@gmail.com>
2016-06-27 21:21:42 -0600

    Stub out methods.

--- src/main/java/spaceage/SpaceAge.java
+++ src/main/java/spaceage/SpaceAge.java
@@ -7,4 +7,44 @@ public class SpaceAge
 	public SpaceAge(Integer ageInSeconds) {
 		throw new NotImplementedException();
 	}
+	
+	public SpaceAge(Long ageInSeconds) {
+		throw new NotImplementedException();
+	}
+	
+	public Double getSeconds() {
+		throw new NotImplementedException();
+	}
+	
+	public Double onMercury() {
+		throw new NotImplementedException();
+	}
+	
+	public Double onVenus() {
+		throw new NotImplementedException();
+	}
+	
+	public Double onEarth() {
+		throw new NotImplementedException();
+	}
+	
+	public Double onMars() {
+		throw new NotImplementedException();
+	}
+	
+	public Double onJupiter() {
+		throw new NotImplementedException();
+	}
+	
+	public Double onSaturn() {
+		throw new NotImplementedException();
+	}
+	
+	public Double onUranus() {
+		throw new NotImplementedException();
+	}
+	
+	public Double onNeptune() {
+		throw new NotImplementedException();
+	}
 }
