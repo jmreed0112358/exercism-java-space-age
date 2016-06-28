b56a248f1dbbf3e0532ad3c49f17939761fc7430
Jeremy M. Reed <reeje76@gmail.com>
2016-06-27 22:03:25 -0600

    Conversion factors to be used.

---
+++ src/main/java/spaceage/ConversionFactors.java
@@ -0,0 +1,13 @@
+package spaceage;
+
+public class ConversionFactors
+{
+	public static final Double SECONDS_PER_EARTH_YEAR = 365.25 * 24 * 60 * 60;
+	public static final Double EARTH_YEARS_PER_MERCURY_YEAR = 0.2408467;
+	public static final Double EARTH_YEARS_PER_VENUS_YEAR = 0.61519726;
+	public static final Double EARTH_YEARS_PER_MARS_YEAR = 1.8808158;
+	public static final Double EARTH_YEARS_PER_JUPITER_YEAR = 11.862615;
+	public static final Double EARTH_YEARS_PER_SATURN_YEAR = 29.447498;
+	public static final Double EARTH_YEARS_PER_URANUS_YEAR = 84.016846;
+	public static final Double EARTH_YEARS_PER_NEPTUNE_YEAR = 164.79132;
+}
