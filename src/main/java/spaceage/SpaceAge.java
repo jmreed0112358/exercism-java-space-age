7e1c9cb0f1d84a344be9a5b1d9cf63f4c353d4fc
Jeremy M. Reed <reeje76@gmail.com>
2016-06-27 21:12:13 -0600

    Project setup.
    Import into Eclipse.
    Convert to maven project.
    Add JUnit library.
    Move sources to proper packages.
    Add NotImplementedException RuntimeException.

---
+++ src/main/java/spaceage/SpaceAge.java
@@ -0,0 +1,10 @@
+package spaceage;
+
+import exceptions.NotImplementedException;
+
+public class SpaceAge
+{
+	public SpaceAge(Integer ageInSeconds) {
+		throw new NotImplementedException();
+	}
+}
--- src/test/java/spaceage/SpaceAgeTest.java
+++ src/test/java/spaceage/SpaceAgeTest.java
@@ -1,3 +1,4 @@
+package spaceage;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
